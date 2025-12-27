c1856429685f1ec143b0d11d7ccb1de3adf45600
chelm <chelmicki.p@gmail.com>
2025-12-27 10:46:35 +0100

    init DB and API

---
+++ API/rest/src/test/java/hamster/dev/fullstack_poc/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package hamster.dev.fullstack_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
