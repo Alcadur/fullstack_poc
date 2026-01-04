2c71f303675cda288445e87c3af51432e897f409
chelm <chelmicki.p@gmail.com>
2026-01-04 23:46:57 +0100

    [Angular] add userHttp service

---
+++ ui/angular/src/app/utils/userHttp.service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserHttpService {
+  private httpClient = inject(HttpClient);
+
+  getDemoUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>("/api/demo-users")
+  }
+
+  login(username: string, password: string): Observable<User> {
+    return this.httpClient.post<User>("/api/login", {username, password})
+  }
+}
