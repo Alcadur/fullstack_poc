77d2827fd1f70b01c6db3b2567b70556ec1510e8
chelm <chelmicki.p@gmail.com>
2026-01-04 20:41:36 +0100

    [API] update security configuration

---
+++ API/rest/src/main/java/hamster/dev/fullstack_poc/config/Config.java
@@ -0,0 +1,36 @@
+package hamster.dev.fullstack_poc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Config {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(
+                                "/api/demo-users",
+                                "/api/login"
+                        )
+                        .permitAll()
+                        .anyRequest().authenticated()
+                );
+        http.cors(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
