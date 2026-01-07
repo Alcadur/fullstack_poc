095af60ea409feb8a1eb98c75588bd67f739717b
chelm <chelmicki.p@gmail.com>
2026-01-07 17:29:02 +0100

    [API] add findAllByAuthor to TaskRepository and us it in service

--- API/rest/src/main/java/hamster/dev/fullstack_poc/repository/TaskRepository.java
+++ API/rest/src/main/java/hamster/dev/fullstack_poc/repository/TaskRepository.java
@@ -1,9 +1,12 @@
 package hamster.dev.fullstack_poc.repository;
 
 import hamster.dev.fullstack_poc.entity.Task;
+import hamster.dev.fullstack_poc.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    Collection<Task> findAllByAuthor(User author);
 }
--- API/rest/src/main/java/hamster/dev/fullstack_poc/service/TaskService.java
+++ API/rest/src/main/java/hamster/dev/fullstack_poc/service/TaskService.java
@@ -1,7 +1,9 @@
 package hamster.dev.fullstack_poc.service;
 
+import hamster.dev.fullstack_poc.dto.TaskDTO;
 import hamster.dev.fullstack_poc.entity.Task;
 import hamster.dev.fullstack_poc.entity.User;
+import hamster.dev.fullstack_poc.mapper.TaskDtoMapper;
 import hamster.dev.fullstack_poc.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TaskService {
     private final TaskRepository taskRepository;
+    private final TaskDtoMapper mapper;
 
     public void createTask(User author, String title, String description) {
         Task task = new Task();
@@ -17,5 +20,9 @@ public class TaskService {
         task.setDescription(description);
         task.setAuthor(author);
         taskRepository.save(task);
+    }
+
+    public TaskDTO[] getAllTasksByAuthor(User user) {
+        return taskRepository.findAllByAuthor(user).stream().map(mapper::toDto).toArray(TaskDTO[]::new);
     }
 }
