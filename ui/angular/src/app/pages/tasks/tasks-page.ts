7d6e5866a447812bffe4b099c57924a9b97a905c
chelm <chelmicki.p@gmail.com>
2026-01-14 00:20:29 +0100

    [Angular] use task list component

--- ui/angular/src/app/pages/tasks/tasks-page.ts
+++ ui/angular/src/app/pages/tasks/tasks-page.ts
@@ -3,12 +3,13 @@ import { TaskStore } from '@/stores/task-store/task-store';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { TopBar } from '@/pages/tasks/components/top-bar/top-bar';
+import { TaskList } from '@/pages/tasks/components/task-list/task-list';
 
 @Component({
   selector: 'tasks-page',
   templateUrl: 'tasks-page.html',
   styleUrls: ['./tasks-page.css'],
-  imports: [MatToolbarModule, MatButtonModule, TopBar],
+  imports: [MatToolbarModule, MatButtonModule, TopBar, TaskList],
   host: { class: 'tasks-page'},
 })
 export class TasksPage {
