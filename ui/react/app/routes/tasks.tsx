20951e5dd7fc28b793f8cf35b30607c0900725cd
chelm <chelmicki.p@gmail.com>
2026-01-24 23:46:32 +0100

    [React] fetch data in task loader

--- ui/react/app/routes/tasks.tsx
+++ ui/react/app/routes/tasks.tsx
@@ -1,10 +1,13 @@
-import type { Route } from "../../.react-router/types/app/routes/+types/tasks";
 import { Tasks } from "@/pages/tasks/tasks";
+import { userSelector } from "@/store/user-slice";
+import { appStore } from "@/store/app-store";
+import { taskHttpService } from "@/service/task-http.service";
 
-export async function clientLoader(a: Route.ClientLoaderArgs) {
-    console.warn("Missing task loader implementation");
+export async function clientLoader() {
+    const user = userSelector(appStore.getState())!;
+    return await taskHttpService.getToDoTasksByUserUid(user.uuid);
 }
 
 export default function TasksRoute() {
-  return <Tasks />
+    return <Tasks />;
 }
