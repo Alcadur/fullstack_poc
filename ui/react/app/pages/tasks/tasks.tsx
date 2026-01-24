136d36100d1f1dd6b964d220ceabb60c14fd39b2
chelm <chelmicki.p@gmail.com>
2026-01-24 23:46:50 +0100

    [React] add tasks list

--- ui/react/app/pages/tasks/tasks.tsx
+++ ui/react/app/pages/tasks/tasks.tsx
@@ -1,3 +1,15 @@
+import { useLoaderData } from "react-router";
+import { List } from "@mui/material";
+import { TaskRow } from "@/pages/tasks/task-row";
+import type { Task } from "@/model/task.model";
+
+// TODO: tests for Tasks
 export function Tasks() {
-    return <div>Tasks</div>
+    const tasks = useLoaderData<Task[]>();
+
+    return (
+        <List className="md:w-2/5 w-full gap-3 flex flex-col" sx={{ marginTop: "12px" }}>
+            {tasks.map((task) => <TaskRow key={task.uuid} task={task} />)}
+        </List>
+    );
 }
