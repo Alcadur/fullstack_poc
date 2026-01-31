60cd737d025cac3649b99bdb1d73ccc6f3c5d967
chelm <chelmicki.p@gmail.com>
2026-01-31 12:41:33 +0100

    [React] update task model

--- ui/react/app/model/task.model.ts
+++ ui/react/app/model/task.model.ts
@@ -1,7 +1,14 @@
+export type Step = {
+    title: string;
+    completed: boolean;
+}
+
+
 export type Task = {
     readonly uuid: string
     readonly authorUuid: string,
     completed: boolean,
     description: string
+    steps: Step[],
     title: string,
 }
