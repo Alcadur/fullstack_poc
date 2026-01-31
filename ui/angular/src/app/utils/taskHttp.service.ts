2f1429e53ecc4f96da8b1fe0fd68826ec85d5fa6
chelm <chelmicki.p@gmail.com>
2026-02-01 00:10:15 +0100

    [Angular] remove user store from taskHttp.service.ts

--- ui/angular/src/app/utils/taskHttp.service.ts
+++ ui/angular/src/app/utils/taskHttp.service.ts
@@ -2,7 +2,6 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Task } from '@/models/task.model';
 import { HttpClient } from '@angular/common/http';
-import { UserStore } from '@/stores/user-store/user-store';
 
 const BASE_URL = '/api/tasks' as const;
 
@@ -11,11 +10,6 @@ const BASE_URL = '/api/tasks' as const;
 })
 export class TaskHttpService {
   private readonly httpClient = inject(HttpClient);
-  private readonly userStore = inject(UserStore);
-
-  private get userUuid() {
-    return this.userStore.user()!.uuid;
-  }
 
   getTasksByCompleted(completed: boolean): Observable<Task[]> {
     return this.httpClient.get<Task[]>(`${BASE_URL}/${completed ? 'completed' : 'todo'}`);
