8bcec437b963e55aa749e7957e9d279163113851
chelm <chelmicki.p@gmail.com>
2026-01-31 12:48:08 +0100

    [React] update task layout to include new task dialog

--- ui/react/app/pages/tasks/tasks-layout.tsx
+++ ui/react/app/pages/tasks/tasks-layout.tsx
@@ -5,10 +5,14 @@ import { AvatarName } from "@/components/avatar-name/avatar-name";
 import { Button } from "@mui/material";
 import { userHttpService } from "@/service/user-http.service";
 import { AppRoutes } from "@/routes/app-routes.model";
+import { useRef } from "react";
+import { NewTaskDialog } from "@/pages/tasks/components/new-task-dialog/new-task-dialog";
+
 
 export default function TasksLayout() {
     const user = useAppSelector(userSelector)!;
     const navigate = useNavigate();
+    const dialogRef = useRef<HTMLDialogElement>(null);
 
     const handleLogout = () => {
         navigate(AppRoutes.LOGIN);
@@ -17,15 +21,23 @@ export default function TasksLayout() {
 
     return (
         <section className="mx-auto md:max-w-250 min-w-75">
+            <NewTaskDialog ref={dialogRef} />
             <header className="flex justify-center gap-3 p-3 shadow-[0_15px_10px_-15px_#111]">
                 <AvatarName username={user.username} />
                 <Button variant="contained" color="error" onClick={handleLogout} data-testid="logout-button">
                     Logout
                 </Button>
             </header>
-            <main className="flex flex-colr justify-center shrink-0">
+            <main className="flex flex-col items-center">
+                <Button
+                    variant="contained"
+                    onClick={() => dialogRef.current?.showModal()}
+                >
+                    Add task
+                </Button>
                 <Outlet />
             </main>
+
         </section>
     );
 }
