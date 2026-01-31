80ee82986ff9adbf11f267bf58e7d7756775d80c
chelm <chelmicki.p@gmail.com>
2026-02-01 00:16:04 +0100

    [Angular] improve the status change button on the task list row

--- ui/angular/src/app/pages/tasks/components/task-list/task-list-row/task-list-row.ts
+++ ui/angular/src/app/pages/tasks/components/task-list/task-list-row/task-list-row.ts
@@ -1,8 +1,11 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { Task } from '@/models/task.model';
 import { TaskStore } from '@/stores/task-store/task-store';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButton } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckbox } from '@angular/material/checkbox';
 
 @Component({
   standalone: true,
@@ -10,18 +13,25 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
   imports: [
     MatExpansionModule,
     MatButtonToggleModule
+    MatButtonToggleModule,
+    MatButton,
   ],
   templateUrl: './task-list-row.html',
   styleUrl: './task-list-row.css',
 })
-export class TaskListRow {
+export class TaskListRow implements OnInit {
   taskStore = inject(TaskStore);
   task = input.required<Task>();
+  localTask?: Task;
+
+  ngOnInit(): void {
+    this.localTask = structuredClone(this.task());
+  }
 
   toggleCompleted(event: MouseEvent) {
     event.stopPropagation();
-    const task = this.task();
-    this.taskStore.toggleTaskCompleted([task.uuid, !task.completed]);
+    const localTask = this.localTask!;
+    localTask.completed = !localTask.completed;
     this.taskStore.updateTask(localTask);
   }
 }
