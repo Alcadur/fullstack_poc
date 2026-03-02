c6dd2df6c641e6038adef82467348f5607690579
chelm <chelmicki.p@gmail.com>
2026-03-02 23:55:17 +0100

    [React] fix mui autocomplete warning during tests

--- ui/react/jest.setup.ts
+++ ui/react/jest.setup.ts
@@ -1,1 +1,25 @@
 import '@testing-library/jest-dom';
+
+const mockRect = {
+  width: 100,
+  height: 20,
+  top: 0,
+  left: 0,
+  bottom: 20,
+  right: 100,
+  x: 0,
+  y: 0,
+  toJSON: () => {},
+};
+
+Object.defineProperty(HTMLElement.prototype, "getBoundingClientRect", {
+  configurable: true,
+  value: () => mockRect,
+});
+
+Object.defineProperty(HTMLElement.prototype, "offsetParent", {
+  configurable: true,
+  get() {
+    return document.body;
+  },
+});
